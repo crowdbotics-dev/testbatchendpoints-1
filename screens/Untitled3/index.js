1fa2c0ae149a550b25dfbf982771d7aac1d75626
Juan B <juan@crowdbotics.com>
2022-12-19 14:26:44 -0300

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled3/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled3 = () => {
+  return <View style={_styles.KIXsqbyO}>
+      </View>;
+};
+
+export default Untitled3;
+
+const _styles = StyleSheet.create({
+  KIXsqbyO: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
