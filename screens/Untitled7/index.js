5f98651057c303be14c1d749a14f7d94d5f531a1
Juan B <juan@crowdbotics.com>
2022-12-26 15:16:29 -0300

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled7/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled7 = () => {
+  return <View style={_styles.FHGBXPqn}>
+      </View>;
+};
+
+export default Untitled7;
+
+const _styles = StyleSheet.create({
+  FHGBXPqn: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
---
+++ screens/Untitled8/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled8 = () => {
+  return <View style={_styles.wvdbwdvv}>
+      </View>;
+};
+
+export default Untitled8;
+
+const _styles = StyleSheet.create({
+  wvdbwdvv: {
+    backgroundColor: "#ffffff",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
